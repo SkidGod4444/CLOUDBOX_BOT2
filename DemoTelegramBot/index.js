056d2e64aae2f2d71532579cc1948715f8d2b07e
Saidev Dhal <dev1974sai@gmail.com>
2024-02-20 08:33:11 +0530

    Testing index.js

--- DemoTelegramBot/index.js
+++ DemoTelegramBot/index.js
@@ -179,7 +179,7 @@ bot.on("message", async (msg) => {
     const msg_id = msg.message_id;
 
     // Send a random message
-    sendRandomMessage(chatId, userInput, msg_id)
+     bot.sendMessage(chatId, chatId)
 });
 
 module.exports = app;
