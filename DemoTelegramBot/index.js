3683158af19b3f1de96a0159b376086bc8a55e85
Saidev Dhal <dev1974sai@gmail.com>
2024-02-20 09:09:52 +0530

    Update index.js

--- DemoTelegramBot/index.js
+++ DemoTelegramBot/index.js
@@ -179,7 +179,8 @@ bot.on("message", async (msg) => {
     const msg_id = msg.message_id;
 
     // Send a random message
-     sendRandomMessage(chatId, userInput, msg_id)
+    bot.sendMessage(chatId, chatId)
+     // sendRandomMessage(chatId, userInput, msg_id)
 });
 
 module.exports = app;
